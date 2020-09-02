c68884dff80b87b4be6bb0429e986d8c78f36fa1
daoyi <1966287146@qq.com>
2020-09-02 09:21:38 +0800

    da

--- js/share.js
+++ js/share.js
@@ -1,6 +1,7 @@
+
+var msg_id;
 $(function() {
     var  layer=layui.layer;
-
 
     $('#codes').click(function () {
         // alert("dsds");
@@ -13,7 +14,8 @@ $(function() {
         if(isnull1){
             var is_phone = isPhone(phone_value);
             if(is_phone){
-                http_code();
+                code_ud();
+                http_code(phone_value);
             }else{
                 layer.msg("请输入正确的手机号");
             }
@@ -25,7 +27,7 @@ $(function() {
     $("#sumbit").click(function () {
          var phone_value = $("#phone").val();
          var code_value = $("#code_e").val();
-         var share_value = getQueryVariable("");
+         var share_value = getQueryVariable("id");
 
         var isnull1 = isnull(phone_value);
         var isnull2=isnull(code_value);
@@ -33,6 +35,7 @@ $(function() {
             var is_phone = isPhone(phone_value);
             if(is_phone){
                 if(isnull2){
+                    http_sumbit(phone_value,code_value,msg_id,share_value);
                     console.log("ddds");
                 }else{
                     layer.msg("验证码不能为空");
@@ -47,6 +50,61 @@ $(function() {
 
     })
 });
+var number=60;
+var css_code_en={
+    "padding": "0",
+    "outline": "none",
+    "background-color": "#32A4FE",
+    "color": "white",
+    "padding": "0px 10px 0px 10px",
+    "box-shadow": "0px 2px 24px 0px #32A4FE",
+    "border-radius": "45px",
+    "height": "30px!important",
+    /*line-height: 30px!important;*/
+    "font-size": "14px",
+    "font-weight": "500",
+    "color":" #FFFFFF",
+    "justify-items": "center",
+}
+var css_code_en_n={
+    "padding": "0",
+    "outline": "none",
+    "background-color": "#9498a1",
+    "color": "white",
+    "padding": "0px 10px 0px 10px",
+    "box-shadow": "0px 2px 24px 0px #32A4FE",
+    "border-radius": "45px",
+    "height": "30px!important",
+    /*line-height: 30px!important;*/
+    "font-size": "14px",
+    "font-weight": "500",
+    "color":" #FFFFFF",
+    "justify-items": "center",
+}
+//验证码
+function code_ud() {
+    var code_up=setInterval(function () {
+        if(number===0){
+            clearInterval(code_up);
+            $("#codes").val("验证码");
+            $("#codes").attr("disabled","false");
+            $("#codes").css(css_code_en);
+            number=60;
+        }else{
+            $("#codes").val("验证码("+number+")")
+            $("#codes").attr("disabled","true");
+            $("#codes").css(css_code_en_n);
+            number--;
+        }
+
+
+    },1000);
+
+}
+
+
+
+
 function isnull(v) {
     if(v==null||v==""){
         return false;
@@ -84,11 +142,13 @@ function http_code(phone) {
         },
         dataType: "json",
         success: function(data){
-
+            console.log(data.data.msg_id);
+            // data.getData().msg_id;
+            msg_id=data.data.msg_id;
         }
     });
 }
-function http_sumbit(phone,code,msg_id) {
+function http_sumbit(phone,code,msg_id,share_value) {
 
     $.ajax({
         type: "POST",
@@ -96,11 +156,13 @@ function http_sumbit(phone,code,msg_id) {
         data: {
             "mobile": phone,
             "code": code,
-            "msg_id": msg_id
+            "msg_id": msg_id,
+            "inviteall": share_value
         },
         dataType: "json",
         success: function(data){
-
+            console.log(data.msg);
+            window.location.href="https://mlqms.shenghuang365.com/mlqm.apk";
         },
         error:function (e) {
 
